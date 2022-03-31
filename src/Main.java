19e114717ce3803e24ec46b1ea97643496174eae
kimanhle102 <kimanhlemuchnok@gmail.com>
2022-03-30 23:57:20 -0400

    add more test cube

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@
 public class Main {
 	public static void main(String[] args) {
 		Solver solver = new Solver(); 
-		
+		// Test Cube 1 with 4 random moves away from the goal cube 
 		System.out.println("Starting cube 1:"); 
 		Cube testCube = new Cube();
+		testCube.cubeScramble(4);
 		System.out.println(testCube.toString()); 
 		solver.BFS(testCube);
 		solver.advancedBFS(testCube);
 		
+		// Test Cube 2 that is 2 Left Move away from the goal cube. 
+		// Since L2 is a move, expected result for the two search is one
 		System.out.println("Starting cube 2: ");  
 		Cube testCube1 = new Cube();
 		testCube1.move("L");
@@ -16,6 +19,9 @@ public class Main {
 		System.out.println(testCube1.toString()); 
 		solver.BFS(testCube1); 
 		solver.advancedBFS(testCube1);
+		
+		
+		//Test Cube 3 
 		
 		
 
